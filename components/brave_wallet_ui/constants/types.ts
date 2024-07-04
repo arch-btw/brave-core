fulldiveVR/e-browser-core

feat: add explore web3 page

components/brave_wallet_ui/constants/types.ts
@@ -624,9 +624,16 @@ export enum WalletRoutes {
   DepositFundsPage = '/crypto/deposit-funds',
   DepositFundsAccountPage = '/crypto/deposit-funds/account',
 
+  // explore
+  Explore = '/crypto/explore',
+
   // market
-  Market = '/crypto/market',
-  MarketSub = '/crypto/market/:chainIdOrMarketSymbol?',
+  Market = '/crypto/explore/market',
+  MarketSub = '/crypto/explore/market/:chainIdOrMarketSymbol?',
+
+  // Web3
+  Web3 = '/crypto/explore/web3',
+  Web3DappDetails = '/crypto/explore/web3/:dappId',
 
   // accounts
   Accounts = '/crypto/accounts',
@@ -961,6 +968,8 @@ export type NavIDTypes =
   | 'transactions'
   | 'my_assets'
   | 'available_assets'
+  | 'explore'
+  | 'web3'
 
 export type AccountPageTabs =
   (typeof AccountPageTabs)[keyof typeof AccountPageTabs]

arch-btw/brave-core

components/brave_wallet_ui/constants/types.ts
@@ -513,9 +513,16 @@ export enum WalletRoutes {
   DepositFundsPage = '/crypto/deposit-funds/:assetId?',
   DepositFundsAccountPage = '/crypto/deposit-funds/:assetId/account',
 
+  // explore
+  Explore = '/crypto/explore',
+
   // market
-  Market = '/crypto/market',
-  MarketSub = '/crypto/market/:coingeckoId?',
+  Market = '/crypto/explore/market',
+  MarketSub = '/crypto/explore/market/:coingeckoId?',
+
+  // Web3
+  Web3 = '/crypto/explore/web3',
+  Web3DappDetails = '/crypto/explore/web3/:dappId',
 
   // accounts
   Accounts = '/crypto/accounts',
@@ -879,6 +886,8 @@ export type NavIDTypes =
   | 'my_assets'
   | 'available_assets'
   | 'bridge'
+  | 'explore'
+  | 'web3'
 
 export type AccountPageTabs =
   (typeof AccountPageTabs)[keyof typeof AccountPageTabs]